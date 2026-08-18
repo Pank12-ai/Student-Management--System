5aca9a61ef27942c642ad61005a3315bd93e2ec1
Pank12-ai <139222754+Pank12-ai@users.noreply.github.com>
2026-08-18 16:39:04 +0530

    Refactor student routes and update authorization

--- Backend/Expressdir/Routes/studentRoutes.js
+++ Backend/Expressdir/Routes/studentRoutes.js
@@ -1,23 +1,23 @@
 const express=require("express");
 const router=express.Router();
 const {
-    getStudents,
-    getStudentById,
-    getMe ,    
-    createStudent,
+    getMyStudent ,
+    getStudents ,
+    updateMyStudent ,
+    getStudentById,   
     updateStudent,
     deleteStudent,
    
 }= require("../Controllers/studentController");
 const auth=require("../Middleware/authMiddleware");
-const authorize = require("../Middleware/roleMiddleware");
+const authorize = require("../Middleware/authorize");
 
-router.get("/",auth,getStudents);
-router.get("/:id",auth,getStudentById);
-router.get("/me",auth,getMe);
-router.post("/",auth,createStudent);
-router.put("/:id",auth,updateStudent);
-router.delete("/:id",auth,deleteStudent);
+//routes
+router.get("/me",auth,getMyStudent);
+router.put("/me",auth,updateMyStudent);
+router.get("/",auth,authorize("admin"),getStudents);
+router.get("/:id",auth,authorize("admin"),getStudentById);
+router.put("/:id",auth,authorize("admin"),updateStudent);
 router.delete("/:id",auth,authorize("admin"),deleteStudent);
 
 
