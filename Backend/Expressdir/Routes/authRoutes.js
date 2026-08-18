2fdee9587a9a25353587c15114b1ad2ec1141657
Pank12-ai <139222754+Pank12-ai@users.noreply.github.com>
2026-08-18 16:35:30 +0530

    Add user routes for getting and updating profile
    Added routes for getting and updating user information.

--- Backend/Expressdir/Routes/authRoutes.js
+++ Backend/Expressdir/Routes/authRoutes.js
@@ -1,9 +1,11 @@
-
 const express=require("express");
 const router=express.Router();
-const {register,login}=require("../Controllers/authController");
-
-
+const {register,login,getMe}=require("../Controllers/authController");
+const auth=require("../Middleware/authMiddleware.js");
+const { updateMyStudent } = require("../Controllers/studentController.js");
+//adding routes 
+router.get("/me", auth, getMe);
+router.put("/me",auth,updateMyStudent);
 router.post("/register",register);
 router.post("/login",login);
 
