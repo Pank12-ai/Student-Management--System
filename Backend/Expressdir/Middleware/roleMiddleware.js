df18f6e7c565e7acff4ef388fec4d453a952a0e6
Pank12-ai <139222754+Pank12-ai@users.noreply.github.com>
2026-08-19 19:57:13 +0530

    Update authorization message for admin access

--- Backend/Expressdir/Middleware/roleMiddleware.js
+++ Backend/Expressdir/Middleware/roleMiddleware.js
@@ -2,7 +2,7 @@ const authorize=(role)=>{
 return (req,res,next)=>{
    if(req.user.role!=role){
     return res.status(403).json({
-        message:"you are not authorized to access this resource"
+        message:"Access denied.Admin Only"
     })
 
    }
